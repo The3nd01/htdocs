5ced672f13a2391751d6f2533826dc071bb95f98
Diego <diegomendigorri@gmail.com>
2024-02-08 19:26:26 +0100

    logo

--- resources/js/react/App.jsx
+++ resources/js/react/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -13,11 +12,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
