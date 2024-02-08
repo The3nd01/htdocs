105137038bbf5a82ce37ffce8a36dade4eec37ea
Diego <diegomendigorri@gmail.com>
2024-02-08 19:42:00 +0100

    sin na de na

--- resources/js/react/App.jsx
+++ resources/js/react/App.jsx
@@ -1,14 +1,8 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  useEffect(()=>{
-    fetch('/api/prueba')
-    .then(respuesta=>respuesta.json())
-    .then(resultado=>console.log(resultado))
-    .catch(error => console.log(error))
-  })
+ 
 
   return (
     <>
