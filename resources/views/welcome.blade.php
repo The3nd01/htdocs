dcca281b35bce5284f3c268a0233c81236c6ad0e
Diego <diegomendigorri@gmail.com>
2024-02-08 19:27:52 +0100

    @vites añadidos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        @viteReactRefresh
+        @vite('resources/js/app.js')
     </head>
     <body class="antialiased">
        <div id="root"></div>
